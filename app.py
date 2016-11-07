8d2b791ae29cfcb7289ed3c297d9f1d79f2ce101
ArchITech <swtdavid@gmail.com>
2016-11-06 20:08:38 -0800

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from time import gmtime, strftime
 @post('/')
 def qtime_down():
 
-	with open("../qtime/restart.log", "a") as myfile:
+	with open("../qtime/src/restart.log", "a") as myfile:
 	    myfile.write(strftime("%Y-%m-%d %H:%M:%S", gmtime())+"\n")
 
 
