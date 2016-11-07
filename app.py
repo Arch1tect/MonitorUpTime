0a1b3d22809d3c30914937e6fdd8a6a97b419903
ArchITech <swtdavid@gmail.com>
2016-11-06 16:30:55 -0800

    .

---
+++ app.py
@@ -0,0 +1,15 @@
+from bottle import post, route, request, run
+from time import gmtime, strftime
+
+
+@post('/')
+def qtime_down():
+
+	with open("../qtime/restart.log", "a") as myfile:
+	    myfile.write(strftime("%Y-%m-%d %H:%M:%S", gmtime())+"\n")
+
+
+	return "succeed!"
+
+
+run(host='0.0.0.0', port=8081)
